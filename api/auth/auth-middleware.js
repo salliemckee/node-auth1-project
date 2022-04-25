16600b9ff9b53e143df494fa7fb7c01841dcf811
Sallie McKee <salliemckee@Morrigan.local>
2022-04-25 16:17:21 -0500

    still troubleshooting middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -48,7 +48,7 @@ async function checkUsernameFree(req, res, next) {
 async function checkUsernameExists(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username });
-    if (!users.length) {
+    if (users) {
       req.user = users[0];
       next();
     } else {
