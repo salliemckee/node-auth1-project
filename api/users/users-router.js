180c86f228c49256f9a3fef8afdb510c62af15d1
Sallie McKee <salliemckee@Morrigan.local>
2022-04-25 12:23:07 -0500

    getting routers, middlewre set up

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,6 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
-
+const router = require("express").Router();
+const { restricted } = require("../auth/auth-middleware");
 /**
   [GET] /api/users
 
@@ -24,5 +24,9 @@
   }
  */
 
-
+router.get("/", restricted, (req, res, next) => {
+  res.json("users");
+});
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+
+module.exports = router;
